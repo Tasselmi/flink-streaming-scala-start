a2cc32ab34ca01bbb92694e9e54e47816a21679f
Tasselmi <tasselmi@yeah.net>
2020-06-30 12:14:39 +0800

    word count socket source

--- src/main/scala/tech/liangfan/flink/WindowWordCount.scala
+++ src/main/scala/tech/liangfan/flink/WindowWordCount.scala
@@ -2,20 +2,29 @@ package tech.liangfan.flink
 
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.api.java.utils.ParameterTool
 
 object WindowWordCount {
-  def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val text = env.socketTextStream("locaohost", 9999)
 
-    val counts = text
-      .flatMap(_.toLowerCase.split("\\W+").filter(_.nonEmpty))
-      .map(s => (s, 1))
-      .keyBy(0)
+  def main(args: Array[String]): Unit = {
+    val params = ParameterTool.fromArgs(args)
+    val hostname = if (params.has("hostname")) params.get("hostname") else "localhost"
+    val port = if (params.has("port")) params.getInt("port") else 9999
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val text: DataStream[String] = env.socketTextStream(hostname, port, '\n')
+
+    val windowCounts = text
+      .flatMap(w => w.split("\\s"))
+      .map(w => WordWithCount(w, 1))
+      .keyBy("word")
       .timeWindow(Time.seconds(5))
-      .sum(1)
-    counts.print()
+      .sum("count")
+
+    windowCounts.print().setParallelism(1)
 
     env.execute("window-word-count")
   }
+
+  case class WordWithCount(word: String, count: Long)
 }
